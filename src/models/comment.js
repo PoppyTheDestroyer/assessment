c1ff793a95f84d9887043102dd0d21b6f44173ad
Jason Saenz <jsigns@gmail.com>
2018-05-20 18:54:46 -0500

    fix

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,30 +1,30 @@
 // create Comment class here
 class Comment {
-    constructor(comment, id) {
-        this.$comment = comment;
-        this.$id = id;
+  constructor(comment, id) {
+    this.$comment = comment;
+    this.$id = id;
 
-        this.findImage(comment, id);
-        this.commentEl(comment, id);
-        this.all();
-    }
+    this.findImage(comment, id);
+    this.commentEl(comment, id);
+    this.all();
+  }
 
-    findImage(comment, id) {
-        //find image object from image.js
-        let commArray = Image.all[id].comments;
-        //push comment into object
-        commArray.push(comment);
-        return Image.all[id];
-    }
+  findImage(comment, id) {
+    //find image object from image.js
+    let commArray = Image.all[id].comments;
+    //push comment into object
+    commArray.push(comment);
+    return Image.all[id];
+  }
 
-    commentEl(comment, id) {
-       return `<li id="newComm-${this.$id}">${this.$comment}</li>`
-    }
-//push new Comment into allComment array
-    all() {
-        this.constructor.allComments.push(this);
-        //console.log(this.constructor.allComments);
-    }
+  commentEl(comment, id) {
+    return `<li id="newComm-${this.$id}">${this.$comment}</li>`;
+  }
+  //push new Comment into allComment array
+  all() {
+    this.constructor.allComments.push(this);
+    //console.log(this.constructor.allComments);
+  }
 }
 
 Comment.allComments = [];
