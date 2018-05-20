29451aa8d4da4c99e4ade25102ad5acf002cbe52
Jason Saenz <jsigns@gmail.com>
2018-05-19 20:08:33 -0500

    comments working

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,18 +1,30 @@
 class CommentsController {
   constructor() {
-    this.$addCommentForm = $('.add-comment');
-    this.$submitComment = $("#submit");
+    this.$addCommentForm = $(".add-comment");
+    this.$submitComment = $(".submit");
     this.$userText = $(".user-text");
   }
 
   init() {
     // kick off controller from here
-    this.$addCommentForm.on("submit", this.addCommentFormListener)
+    this.addCommentFormListener();
   }
 
-  addCommentFormListener(e) {
-    e.preventDefault();
-    console.log("OK");
+  addCommentFormListener() {
+    this.$submitComment.on("click", event => {
+      event.preventDefault();
+      let id = event.target.id;
+      let comment = this.$userText[id].value;
+      let commentNew = new Comment(id, comment);
+      //find image ID - set same as input ID
+      let imageID = `image-${id}`
+      this.render(comment, id);
+      this.$addCommentForm = "";
+    });
+  }
 
+  render(comment, id) {
+    //select correct ul
+    $(`#comments-${id}`).append(`<li>${comment}</li>`)
   }
 }
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,4 +1,7 @@
 // create Comment class here
 class Comment {
-    
+    constuctor(id, value) {
+        this.$id = id;
+        this.$comment = value;
+    }
 }
