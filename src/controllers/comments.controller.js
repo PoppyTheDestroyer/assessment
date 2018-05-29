41cfff14fa562efc882e90e16d55ac3987c44f93
Jason Saenz <jsigns@gmail.com>
2018-05-29 09:42:14 -0500

    note

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -24,6 +24,7 @@ class CommentsController {
       let commentNew = new Comment(comment, id);
       // console.log(commentNew);
       this.render(commentNew);
+      //Resets form after submission
       document.getElementById(`add-comment-${id}`).reset();
     });
   }
